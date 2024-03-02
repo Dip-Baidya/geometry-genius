6186012f2e1f56c25a12570036971b2be9f5516b
Dip-Baidya <dipbaidya.cse@gmail.com>
2024-03-03 01:43:23 +0600

    26-5 (Recap) Calculate Rectangle Area And Display

--- script/rectangle-area.js
+++ script/rectangle-area.js
@@ -1,3 +1,21 @@
-function calculateRectAngleArea(){
+function calculateRectAngleArea() {
+    // Find Length
+    const lenghthInput = document.getElementById('rectangle-length');
+    const rectAngleLengthText = lenghthInput.value;
+    length = parseFloat(rectAngleLengthText);
+
+    // Find Width
+    const widthInput = document.getElementById('rectangle-height');
+    const rectangleHeightText = widthInput.value;
+    width = parseFloat(rectangleHeightText);
+
+    // Final Calculation
+
+    const area = length * width;
+
+    // Display Area
+
+    const rectangleAreaSpan = document.getElementById('rectangle-area');
+    rectangleAreaSpan.innerText = area;
 
 }
